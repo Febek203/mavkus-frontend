38322191dcc78821cb07aaae92086eb275e3d87a
Federico Grumberg <Febek203@gmail.com>
2025-12-19 13:02:51 +0100

    Aggiornamento firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -15,7 +15,7 @@ import {
   doc, 
   setDoc, 
   getDoc, 
-  updateDoc
+  updateDoca
 } from "firebase/firestore";
 
 // ============================================
@@ -23,7 +23,7 @@ import {
 // ============================================
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_FIREaBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
